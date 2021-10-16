53a0307fc964cf46d9c78e8cfde6f8b51e758d0f
KrisZdrav <90221092+KrisZdrav@users.noreply.github.com>
2021-10-16 15:45:26 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+
+
+
+/*
+
+========This function is used to calculate value from inch to cm.==========
+
+function calculate(value){
+    const newValue = value * 2.54;
+    console.log('The value in cm is : ' + value * 2.54 + ' cm');
+    return value * 2.54;
+
+}
+
+const wallHeight = 80;
+
+const width = calculate(100);
+const height = calculate(wallHeight);
+
+const dimension = [width, height]
+
+console.log(dimension); */
+
+
+
+/* 
+
+========Practice of IF and ELSE function===========
+
+const value = 1>1;
+if (value) {
+    console.log('Hello world');
+}
+else {
+    console.log('Hello people');
+}  */
+
+
+
+/*  ========= LOOP PRACTICE ============
+let amount = 10;
+while (amount > 0) {
+    console.log('I have ' + amount + "  dollars and im going to the mall")
+    amount--
+} */
+
+
+/*
+let money = 0;
+do {
+    console.log('You have ' + money + ' dollars');
+    money++;
+} while (money < 10) 
+
+let i;
+for (i=0; i<10; i++) {
+    console.log('and the number is : ' + i);
+}
+*/
